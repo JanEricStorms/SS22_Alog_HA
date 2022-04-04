459f8a79fd51c0ff8ddb595aa3b87802e885cbf8
ZyklopRT <j02.jansen@icloud.com>
2022-04-04 16:11:04 +0200

    coding standards

--- src/ha/jan/janha021/MyHashSet.java
+++ src/ha/jan/janha021/MyHashSet.java
@@ -7,7 +7,8 @@ import java.util.Hashtable;
  * @author Svenja, Jan-Eric, Justin
  * @param <K> > Type of HashSet (z.B. Integer, String
  */
-public class MyHashSet<K>{
+public class MyHashSet<K>
+{
 
     private ArrayList<K>[] myHash;
     private int numberOfElements;
@@ -17,7 +18,8 @@ public class MyHashSet<K>{
      * Konstruktor der Klasse {@link MyHashSet} </br>
      * Erzeugt die HashTabelle, setzt die numberOfElements auf 0, und die initzial groesse von Size auf 10 </br>
      */
-    public MyHashSet(){
+    public MyHashSet()
+    {
         myHash = new ArrayList[10];
         numberOfElements = 0;
         size = 10;
@@ -28,8 +30,9 @@ public class MyHashSet<K>{
      * Prueft den Fuellgrad der HashTabelle, </br>
      * falls dieser groeßer als 2 ist wird die HashTabelle rebased mit size*2
      */
-    private void checkFillLvl(){
-        if((numberOfElements+1)/size >= 2){
+    private void checkFillLvl()
+    {
+        if ((numberOfElements+1)/size >= 2) {
             rebaseMyHash();
         }
     }
@@ -39,18 +42,17 @@ public class MyHashSet<K>{
      * vergroessert den Wert size auf das doppelte der HashTabelle und cleared die HashTabelle. </br>
      * Danach wird die HashTabelle initialisiert und die Werte in die neue HashTabelle einsortiert.
      */
-    private void rebaseMyHash(){
+    private void rebaseMyHash()
+    {
         ArrayList<K> allValues = getElements();
         size = 2*myHash.length;
         myHash = new ArrayList[size];
         initializeHashtable();
         int hashCode;
-        for(int i = 0; i < allValues.size();i++){
+        for (int i = 0; i < allValues.size();i++) {
             hashCode = allValues.get(i).hashCode()%size;
             this.myHash[hashCode].add(allValues.get(i));
         }
-
-
     }
 
     /**
@@ -58,10 +60,11 @@ public class MyHashSet<K>{
      * @param element > Element des Typ K </br>
      * @return boolean > True wenn das Element schon enthalten ist, false wenn das Element neu ist. </br>
      */
-    public boolean add(K element){
+    public boolean add(K element)
+    {
         checkFillLvl();
         int hashCode = Math.abs(element.hashCode()%size);
-        if(contains(element)) {
+        if (contains(element)) {
             return true;
         }
         this.myHash[hashCode].add(element);
@@ -75,9 +78,10 @@ public class MyHashSet<K>{
      * @param element > Element des Typ K </br>
      * @return boolean > True wenn das Element enthalten ist, false wenn das Element nicht enthalten ist. </br>
      */
-    public boolean delete(K element){
+    public boolean delete(K element)
+    {
         int hashCode = Math.abs(element.hashCode()%size);
-        if(contains(element)){
+        if (contains(element)) {
             this.myHash[hashCode].remove(element);
             return true;
         }
@@ -90,9 +94,10 @@ public class MyHashSet<K>{
      * @param element > Element des Typ K </br>
      * @return boolean > True wenn das Element enthalten ist, false wenn das Element nicht enthalten ist. </br>
      */
-    public boolean contains(K element ){
+    public boolean contains(K element )
+    {
         int hashCode = Math.abs(element.hashCode()%size);
-        if(this.myHash[hashCode].contains(element)){
+        if (this.myHash[hashCode].contains(element)) {
             return true;
         }
         return false;
@@ -103,11 +108,12 @@ public class MyHashSet<K>{
      * Gibt eine ArrayList< K > zurueck die alle Elemente der HashTabelle enthaelt
      * @return ArrayList > Enthaelt alle Elemente der HashTabelle
      */
-    public ArrayList<K> getElements(){
+    public ArrayList<K> getElements()
+    {
         ArrayList<K> newList = new ArrayList<>();
         for (int i = 0; i < size;i++){
             ArrayList<K> list = this.myHash[i];
-            for(int j = 0; j < list.size(); j++){
+            for (int j = 0; j < list.size(); j++) {
                 newList.add(list.get(j));
             }
         }
@@ -117,8 +123,9 @@ public class MyHashSet<K>{
     /**
      * Initialisiert die HashTabelle
      */
-    private void initializeHashtable(){
-        for (int i = 0; i < size;i++){
+    private void initializeHashtable()
+    {
+        for (int i = 0; i < size;i++) {
             this.myHash[i] = new ArrayList<K>();
         }
     }
